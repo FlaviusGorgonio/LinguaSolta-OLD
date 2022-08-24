910aa89223f4e5ef8e7799133a81077d24fa0ca8
Prof. Flavius Gorgonio <flavius.gorgonio@ufrn.br>
2022-08-24 01:13:28 -0300

    Cria tela principal

--- ls.c
+++ ls.c
@@ -1,6 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///             Developed by @flgorgonio -- since Out, 2021                 ///
+///////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 
-int main(void){
-    printf("Programa Lingua Solta\n");
+/////
+// Assinatura das funções
+void telaPrincipal(void);
+
+/////
+// Programa principal
+int main(void) {
+    telaPrincipal();
     return 0;
+}
+
+
+
+void telaPrincipal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+    printf("///             Developed by @flgorgonio -- since Out, 2021                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Gestão Escolar = = = = =                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Módulo Aluno                                              ///\n");
+    printf("///            2. Módulo Professor                                          ///\n");
+    printf("///            3. Módulo Turma                                              ///\n");
+    printf("///            4. Módulo Matrícula                                          ///\n");
+    printf("///            5. Módulo Relatórios                                         ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
 }
