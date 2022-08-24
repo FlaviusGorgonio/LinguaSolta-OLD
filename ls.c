d9cd6826fec8200700ac6050ef27fcd9eb1d98e2
Prof. Flavius Gorgonio <flavius.gorgonio@ufrn.br>
2022-08-24 01:25:07 -0300

    Cria tela equipe

--- ls.c
+++ ls.c
@@ -13,19 +13,20 @@
 // Assinatura das funções
 void tela_principal(void);
 void tela_sobre(void);
+void tela_equipe(void);
 
 /////
 // Programa principal
 int main(void) {
     tela_principal();
     tela_sobre();
+    tela_equipe();
     return 0;
 }
 
 
 /////
 // Funções
-
 
 void tela_principal(void) {
     printf("\n");
@@ -78,3 +79,30 @@ void tela_sobre(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 }
+
+
+void tela_equipe(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+    printf("///             Developed by @flgorgonio -- since Jan, 2021                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Gestão Escolar = = = = =                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Este projeto exemplo foi desenvolvido por:                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Prof. Flavius Gorgônio                                       ///\n");
+    printf("///            E-mail: flavius.gorgonio@ufrn.br                             ///\n");
+    printf("///            Redes sociais: @flgorgonio                                   ///\n");
+    printf("///            Git: https://github.com/FlaviusGorgonio/LinguaSolta.git      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+} 
